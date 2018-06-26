2cff9a5162a3a033c33c949fcd4b4afcff602804
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-06-26 12:46:20 +0200

    navbar created

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php require_once("config/includes_head.php"); ?>
     <title>Biofiszki</title>
 </head>
 <body>
+    <?php require_once("navbar.php"); ?>
     
+
+
+    <?php require_once("config/includes_body.php"); ?>
 </body>
 </html>
